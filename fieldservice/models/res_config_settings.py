f834e6fdbf39f2a1f5e9d9be5af8d2e61345bb6b
brian10048 <brian@mcmpest.com>
2019-10-16 14:31:29 -0400

    [ADD] fieldservice_fleet

--- fieldservice/models/res_config_settings.py
+++ fieldservice/models/res_config_settings.py
@@ -55,6 +55,8 @@ class ResConfigSettings(models.TransientModel):
         string='Projects and Tasks')
     module_fieldservice_crm = fields.Boolean(
         string='CRM')
+    module_fieldservice_fleet = fields.Boolean(
+        string='Link to Fleet Vehicles')
 
     # Companies
     auto_populate_persons_on_location = fields.Boolean(
